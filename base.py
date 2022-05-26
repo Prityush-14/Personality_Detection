0d9c0453baeb8c05b0f2abb07ea22217ca264bd4
Prityush-14 <bprityush14@gmail.com>
2022-05-26 18:08:06 +0530

    Started to analyze the dataset

--- base.py
+++ base.py
@@ -1,1 +1,13 @@
-# This is just the base starter file
+# Author : Prityush Bansal
+# May 26, 2022 : 16:24
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+print("Dataframe head is \n", df.head())
+print("\n\nInfo for dataframe is as follows : ")
+print(df.info())
+print("\n\nCount for each type :\n", df['type'].value_counts())
+# so there are no null values in the dataset, but our dataset is very
+# imbalanced which can be seen through the value count
